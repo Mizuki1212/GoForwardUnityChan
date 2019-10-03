ae7952022c80df2f64fa1ceb0377367a96050f35
takafumisasaki <taketakakun22@yahoo.co.jp>
2019-10-04 03:11:23 +0900

    Lesson7 GoForwardUnityChan のコミット
    ＜課題＞キューブの接触時に効果音をつけましょう
    ・キューブが地面に接触する時とキューブ同士が積み重なるときに音声ファイル「block」使用する

---
+++ Assets/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+	
+	// キューブの移動速度（左方向）
+	private float speed = -0.2f;
+	// 消滅位置
+	private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// キューブを左方向に移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 左方向へ持っていき画面外へ消えたら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Sound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+    // 効果音を鳴らす時はAudioClip変数を宣言
+    public AudioClip blockSE;
+
+    AudioSource sound;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+
+    }
+
+        // 練習 デバッグログで接触を調べる
+        void OnCollisionEnter2D(Collision2D collision2D)
+        {
+            Debug.Log("オブジェクト衝突: " + collision2D.gameObject.name);
+        
+        // CubePrefabが以下のタグに接触した場合
+        if (collision2D.gameObject.tag == "Cube" || collision2D.gameObject.tag == "ground")
+        {
+            // SE再生 ※SEは重ねて再生する場合があるので PlayOneShot() を使う
+            sound.PlayOneShot(blockSE);
+
+            // 練習 デバッグログでSE再生を調べる
+            Debug.Log("SE再生: " + blockSE.name);
+        }
+
+        }
+    }
